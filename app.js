5314b324650473ff11d5b64fa97bbecce293f0b6
JOELMA GOMES DA SILVA <2022317668@MACEIO.IFAL>
2024-02-26 15:28:04 -0300

    atualizando

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+const multer = require('multer');
+const bodyParser = require('body-parser');
+
+const upload = multer();
+
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+app.get('/cadastro', (req, res) => {
+    res.render('cadastro');
+})
+app.get('/usuario', (req, res) => {
+    res.render('usuario');
+})
+
+app.listen(2000);
+
+console.log('Sistema iniciado no port: 2000');
